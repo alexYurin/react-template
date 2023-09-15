33f8575a6608e6057bf64602fc6befcd6e4eb8da
alexYurin <sans.y@mail.ru>
2023-09-15 12:48:54 +0700

    fixed json-server route

--- json.server.cjs
+++ json.server.cjs
@@ -7,7 +7,7 @@ const middleware = jsonServer.defaults()
 const JSON_SERVER_PORT = process.env.VITE_JSON_SERVER_PORT
 
 const customRoutes = {
-  '/users': '/users',
+  '/features': '/features',
 }
 
 server.use(middleware)
@@ -17,5 +17,5 @@ server.use(jsonServer.rewriter(customRoutes))
 server.use(router)
 
 server.listen(JSON_SERVER_PORT, () => {
-  console.log('JSON Server is running on port ' + JSON_SERVER_PORT)
+  console.log('🎸 JSON Server is running on port ' + JSON_SERVER_PORT)
 })
